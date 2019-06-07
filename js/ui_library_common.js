15deca20e95ffd0c4affc359dad4928d36e00258
yohey7779 <49703007+yohey7779@users.noreply.github.com>
2019-06-07 12:17:35 +0900

    20190607_Test

---
+++ js/ui_library_common.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+$(document).ready(function(){
+    preventDefaultAnchor();
+    
+    
+});
+
+
+function preventDefaultAnchor() {
+    $(document).on('click', 'a[href="#"]', function(e) {
+        e.preventDefault();
+    });
+}
+
